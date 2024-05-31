777996a0fa6b9890a7a81e58c41b67757b2c7812
Quoc Quan Robert Trinh <qtrinh@f1r1s2.codam.nl>
2024-05-31 13:07:13 +0200

    bugfix/#58 unset leak fix

--- sources/builtins/unset.c
+++ sources/builtins/unset.c
@@ -6,66 +6,40 @@
 /*   By: quentinbeukelman <quentinbeukelman@stud      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/13 21:02:51 by quentinbeuk   #+#    #+#                 */
-/*   Updated: 2024/05/16 17:22:04 by qtrinh        ########   odam.nl         */
+/*   Updated: 2024/05/31 13:06:49 by qtrinh        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static char	**buffer_env_after_index(char **env, int index)
+static void	remove_line_from_env(char **env, const char *var)
 {
-	int		i;
-	int		buffer_len;
-	char	**buffer;
+	int	i;
+	int	len;
 
 	i = 0;
-	buffer_len = count_lines_from(env, index) + 1;
-	buffer = (char **)safe_malloc(sizeof(char *) * buffer_len);
-	while (env[index])
+	len = ft_strlen(var);
+	while (env[i])
 	{
-		buffer[i] = safe_strdup(env[index]);
+		if (ft_strncmp(env[i], var, len) == 0 &&
+			env[i][len] == '=')
+		{
+			free(env[i]);
+			while (env[i + 1])
+			{
+				env[i] = env[i + 1];
+				i++;
+			}
+			env[i] = NULL;
+			return ;
+		}
 		i++;
-		index++;
-	}
-	buffer[i] = NULL;
-	return (buffer);
-}
-
-static void	insert_env_buffer(char **env, char **buffer, int env_index)
-{
-	int		buffer_index;
-
-	buffer_index = 0;
-	while (env[env_index])
-	{
-		env[env_index] = buffer[buffer_index];
-		buffer_index++;
-		env_index++;
-	}
-	env[env_index + 1] = NULL;
-}
-
-static void	remove_line_from_env(char **env, char *key)
-{
-	int		remove_index;
-	char	**buffer;
-
-	remove_index = index_for_env_key(env, key);
-	if (remove_index >= 0)
-	{
-		buffer = buffer_env_after_index(env, (remove_index + 1));
-		insert_env_buffer(env, buffer, remove_index);
-		// if (buffer)
-		// 	free_2d_array(buffer); // * double check if correct free!
 	}
 }
 
-// TODO error checks -> not a valid identifier
-// TODO add to test set ->	unset HOME PATH SHELL USER PWD TERM ZSH
 int	unset(t_cmd* cmd, t_shell *shell)
 {
-	int		i;
-	int		new_size;
+	int	i;
 
 	i = 0;
 	while (i < cmd->arg_count)
@@ -73,7 +47,5 @@ int	unset(t_cmd* cmd, t_shell *shell)
 		remove_line_from_env(shell->envp, cmd->args[i]);
 		i++;
 	}
-	new_size = env_size(shell->envp);
-	shell->envp = ft_realloc(shell->envp, new_size);
 	return (0);
 }
