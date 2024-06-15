4c9181bd00c702841ef9d6dd31a2e16eb474e327
QBeukelman <quentin.nextresearch@gmail.com>
2024-06-15 12:53:14 +0200

    feature/#115 Add vector functions to libft

---
+++ includes/libft/lib_vector/vector.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   vector.h                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: quentinbeukelman <quentinbeukelman@stud      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/06/15 12:38:45 by quentinbeuk   #+#    #+#                 */
+/*   Updated: 2024/06/15 12:42:32 by quentinbeuk   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_H
+# define VECTOR_H
+
+#include "../includes/libft.h"
+
+typedef struct s_vec {
+	char *data;
+	unsigned int len;
+	unsigned int capacity;
+}	t_vec;
+
+// ft_vector.c
+void	ft_free_vector(t_vec *v);
+bool	ft_vec_init(t_vec *v, unsigned int size);
+bool	ft_vec_resize(t_vec *v);
+bool	ft_vec_push(t_vec *v, char c);
+char	ft_*get_str(t_vec *v);
+
+#endif
